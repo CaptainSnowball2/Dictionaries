0aa216da8e4c746922650999f9947cb7ddc11ff8
CaptainSnowball2 <arno.kruger2013@outlook.com>
2025-11-17 17:59:01 +0100

    Add files via upload

---
+++ lesson 1.py
@@ -0,0 +1,8 @@
+C={"HackPack":"toys you can code","Lego":"Building blocks"}
+C["chocolate"]="coco bar" 
+A=input("What do you want? ")
+if "UK" in C:
+    print("no")
+else:
+    print("NO!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+print(C[A])
